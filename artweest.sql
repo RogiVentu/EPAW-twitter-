0e0b6fd04c19c31a9fbf18ae81fbdbbf484bb76a
Alexis Ruiz <alexis.ruiz01@estudiant.upf.edu>
2017-06-27 20:25:02 +0200

    new database

--- artweest.sql
+++ artweest.sql
@@ -29,8 +29,8 @@ CREATE TABLE `follows` (
   `byUser` varchar(255) NOT NULL,
   PRIMARY KEY (`followed`,`byUser`),
   KEY `byUser` (`byUser`),
-  CONSTRAINT `follows_ibfk_1` FOREIGN KEY (`followed`) REFERENCES `users` (`username`),
-  CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`byUser`) REFERENCES `users` (`username`)
+  CONSTRAINT `follows_ibfk_1` FOREIGN KEY (`followed`) REFERENCES `users` (`username`) ON DELETE CASCADE,
+  CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`byUser`) REFERENCES `users` (`username`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -49,7 +49,7 @@ CREATE TABLE `tweets` (
   `picture` longblob,
   PRIMARY KEY (`id`),
   KEY `user` (`user`),
-  CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`username`)
+  CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`username`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -57,9 +57,9 @@ LOCK TABLES `tweets` WRITE;
 INSERT INTO `tweets` (`title`,`text`,`user`,`time`,`picture`) VALUES ('Cach Me Outside','Just come outside and catch me man u know u cant','Danielle','14-06-2017 17:34:00',NULL),('I hate mondays','OH.. Today is monday... Please kill me mates','Danielle','13-06-2017 18:52:34',NULL),('Restaurant Gourmet','El restaurant de la Garriga anomenat Jamelexirogi està molt bo, fan uns plats molt gourmets','laieee','14-06-2017 02:23:14',NULL),('Comença l\'estiu, per fí!','Per fi he acabat els examens ara cap a la platja!','laieee','12-06-2017 11:23:14',NULL),('I am the best','We deserve that win, and i deserve the mpv title in finals because i did it all, my team did zero work.','Durant','13-06-2017 09:23:14',NULL),('I love you Lebron','Just right now i found Lebron James in one party, he is a good boy at all, i think i feel in love with him','Durant','15-06-2017 00:33:51',NULL),('I can\'t resist this','Oh guys, im sorry i cant resist go to bed with Lebron, he is an amazing bro','Durant','15-06-2017 03:53:12',NULL),('Funsiona?','Si lo ase es increible bro','Durant','18-06-3024 04:53:12',NULL);
 UNLOCK TABLES;
 
-SELECT * FROM users;
+#SELECT * FROM users;
 #SELECT * FROM tweets;
-SELECT * FROM follows;
+#SELECT * FROM follows;
 #SELECT title, text , user, time FROM tweets T, follows F WHERE F.byUser = 'Hancock' AND T.user = F.followed;
 #SELECT username FROM users WHERE username like'd%'ORDER BY username;
 #SELECT name, surnames, gender, datebirth, username, email FROM users WHERE username = 'Hancock';
